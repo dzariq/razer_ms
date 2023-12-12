bb84c3603c39eaba521d64c5187611a484aff70b
Dzariq <dzariq85@gmail.com>
2023-12-12 17:17:19 +0800

    a

---
+++ laravel/app/Http/Controllers/PaymentHistoryController.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\payment_channel_response;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Crypt;
+use Log;
+
+class PaymentHistoryController extends Controller
+{
+    public function __construct()
+    {
+    }
+
+    public function index(Request $request)
+    {
+        $orderId = $request->get('transaction_reference');
+        $domain = env("RAZER_MERCHANT_ID");
+
+        $histories = payment_channel_response::where("transaction_reference", $orderId)->get();
+
+        return json_encode($histories);
+
+
+
+
+    }
+}
